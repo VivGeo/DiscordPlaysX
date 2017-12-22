8396e81504ed673b8021e01f39adabae720f9869
vivgeo <vivek.george@ryerson.ca>
2017-12-22 16:14:32 -0500

    add ds controls with ability to switch between ds and gba

--- src/main/java/AnnotationListener.java
+++ src/main/java/AnnotationListener.java
@@ -26,62 +26,100 @@ public class AnnotationListener {
         }
         */
     @EventSubscriber
-    public void onMessageReceivedEvent(MessageReceivedEvent event) { // This method is NOT called because it doesn't have the @EventSubscriber annotation
+    public void onMessageReceivedEvent(MessageReceivedEvent event) throws InterruptedException { // This method is NOT called because it doesn't have the @EventSubscriber annotation
         IMessage message = null;
+        int key = 0;
+        boolean pressed = false;
         try {
         message = event.getMessage();
-
         if (!message.getChannel().getName().equals("stuff_and_things"))
             return;
 
-        String content = message.getContent();
+        String content = message.getContent().toLowerCase();
         if (content.equals("up")) { //u d l r a b x y select //LS RS
-            robot.keyPress(KeyEvent.VK_UP);
-            robot.keyRelease(KeyEvent.VK_UP);
+            key = KeyEvent.VK_UP;
+            pressed = true;
         }
         else if (content.equals("down")) {
-            robot.keyPress(KeyEvent.VK_DOWN);
-            robot.keyRelease(KeyEvent.VK_DOWN);
+            key = KeyEvent.VK_DOWN;
+            pressed = true;
         }
         else if (content.equals("left")) {
-            robot.keyPress(KeyEvent.VK_LEFT);
-            robot.keyRelease(KeyEvent.VK_LEFT);
+            key = KeyEvent.VK_LEFT;
+            pressed = true;
         }
         else if (content.equals("right")) {
-            robot.keyPress(KeyEvent.VK_RIGHT);
-            robot.keyRelease(KeyEvent.VK_RIGHT);
-        }
-        else if (content.equals("a")) {
-            robot.keyPress(KeyEvent.VK_X);
-            robot.keyRelease(KeyEvent.VK_X);
-        }
-        else if (content.equals("b")) {
-            robot.keyPress(KeyEvent.VK_Z);
-            robot.keyRelease(KeyEvent.VK_Z);
-        }
-        else if (content.equals("ls")) {
-            robot.keyPress(KeyEvent.VK_A);
-            robot.keyRelease(KeyEvent.VK_A);
-        }
-        else if (content.equals("rs")) {
-            robot.keyPress(KeyEvent.VK_S);
-            robot.keyRelease(KeyEvent.VK_S);
+            key = KeyEvent.VK_RIGHT;
+            pressed = true;
         }
         /*
-        else if (content.equals("y")) {
-            robot.keyPress(KeyEvent.VK_Y);
-            robot.keyRelease(KeyEvent.VK_Y);
+        else if (content.equals("a")) {
+            key = KeyEvent.VK_X;
+            pressed = true;
         }
-        */
-        else if (content.equals("select")) {
-            robot.keyPress(KeyEvent.VK_BACK_SPACE);
-            robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+        else if (content.equals("b")) {
+            key = KeyEvent.VK_Z;
+            pressed = true;
         }
-        else if (content.equals("start")) {
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
+        else if (content.equals("ls")) {
+            key = KeyEvent.VK_A;
+
+        }
+        else if (content.equals("rs")) {
+            key = KeyEvent.VK_S;
+            pressed = true;
         }
 
+        else if (content.equals("select")) {
+            key = KeyEvent.VK_BACK_SPACE;
+            pressed = true;
+        }
+        else if (content.equals("start")) {
+            key = KeyEvent.VK_ENTER;
+            pressed = true;
+        }
+        */ //DS controls - ABYYX, START SELECT, L R
+        else if (content.equals("a")) {
+            key = KeyEvent.VK_X;
+            pressed = true;
+        }
+        else if (content.equals("b")) {
+            key = KeyEvent.VK_Z;
+            pressed = true;
+        }
+        else if (content.equals("y")) {
+            key = KeyEvent.VK_A;
+            pressed = true;
+        }
+        else if (content.equals("X")) {
+            key = KeyEvent.VK_S;
+            pressed = true;
+        }
+        else if (content.equals("start")) {
+            key = KeyEvent.VK_ENTER;
+            pressed = true;
+        }
+        else if (content.equals("select")) {
+            key = KeyEvent.VK_V;
+            pressed = true;
+        }
+        else if (content.equals("l")) {
+            key = KeyEvent.VK_Q;
+            pressed = true;
+        }
+        else if (content.equals("r")) {
+            key = KeyEvent.VK_W;
+            pressed = true;
+        }
+        // */
+        if (pressed) {
+            robot.keyPress(key);
+            Thread.sleep(150);
+            robot.keyRelease(key);
+        }
+        //robot.keyPress(KeyEvent.VK_ENTER);
+            //Thread.sleep(150);
+            // robot.keyRelease(KeyEvent.VK_ENTER);
         /*(if (content.startsWith("!darren"))
         {
             new MessageBuilder(client).appendContent("\uD83D\uDE93 WEE OOO WEE OO \uD83D\uDE93\n\uD83D\uDC6E SEX CRIME DETECTED \uD83D\uDC6E").withChannel(message.getChannel()).send();
@@ -93,6 +131,11 @@ public class AnnotationListener {
 
         catch (RateLimitException e) {
             new MessageBuilder(client).appendContent("**Rate Limit Reached**.").withChannel(message.getChannel()).send();
+        }
+        catch (Exception e) {
+            new MessageBuilder(client).appendContent("**Exception**.").withChannel(message.getChannel()).send();
+            e.printStackTrace();
+
         }
     }
 
